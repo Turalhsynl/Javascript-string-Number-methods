3d807caaab2f20bdfc7eb6fbc1da8233c4951c55
Huseynli Tural Zeka <Husey_vw08@itstep.edu.az>
2024-10-31 16:46:53 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+//Task 1
+
+// let str = "hello";
+// console.log(str[0].toUpperCase() + str.slice(1));
+
+//Task 2
+// let str = "Hello World";
+
+// function checkSubstr(text, inputText){
+//     return text.includes(inputText);
+// };
+
+// console.log(checkSubstr(str,"World"));
+
+//Task 3
+// let text = "salam"
+
+// function repeatt(text,n){
+//     return text.repeat(n);
+// }
+
+// console.log(repeatt(text,5));
+
+//Task 4
+
+// let str  = "Hello this is StepIT Academy";
+
+// function replaceDemo(str,text_1,text_2){
+//     return str.replaceAll(text_1,text_2);
+// }
+
+// console.log(replaceDemo(str,"Hello","Salam"));
+
+//Task 5
+// let str  = "Hello this is StepIT Academy";
+
+// function countWord(str){
+//     return str.split(" ").length
+// }
+
+// console.log(countWord(str));
+
+//Task 6
+// let text = "Hello";
+
+// function reverseChar(text){
+//     return text.split("").reverse().join("");
+// }
+// console.log(reverseChar(text));
+
+//Task 7
+
+
